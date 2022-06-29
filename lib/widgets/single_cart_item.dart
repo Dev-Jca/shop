490637b2f9a16cde3eae53ef71cea75451baab54
J Charles Adebola <writingjca@gmail.com>
2022-06-29 08:59:26 +0100

    step-5

--- lib/widgets/single_cart_item.dart
+++ lib/widgets/single_cart_item.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart_provider.dart';
 
 class SingleCartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
   const SingleCartItem(
       {required this.id,
+      required this.productId,
       required this.title,
       required this.price,
       required this.quantity,
@@ -16,10 +21,29 @@ class SingleCartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
     return Dismissible(
       key: ValueKey(id),
+      background: Container(
+        color: Colors.grey.withOpacity(0.1),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.black38,
+          size: 40,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        cart.removeItem(productId);
+      },
       child: Card(
-        margin: EdgeInsets.symmetric(
+        margin: const EdgeInsets.symmetric(
           horizontal: 15,
           vertical: 4,
         ),
